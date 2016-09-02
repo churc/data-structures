8becc6c355f509fdb9fa71551ec7947fd32ab187
churc <ccinflux@hotmail.com>
2016-09-02 17:22:23 -0400

    Add files via upload

---
+++ homework1_churc/app1.js
@@ -0,0 +1,73 @@
+var request = require('request');
+var fs = require('fs');
+
+request('http://visualizedata.github.io/datastructures/data/m01.html', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    fs.writeFileSync('/home/ubuntu/workspace/homework/101.txt', body);
+  }
+  else {console.error('request failed')}
+})
+
+request('http://visualizedata.github.io/datastructures/data/m02.html', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    fs.writeFileSync('/home/ubuntu/workspace/homework/102.txt', body);
+  }
+  else {console.error('request failed')}
+})
+
+request('http://visualizedata.github.io/datastructures/data/m03.html', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    fs.writeFileSync('/home/ubuntu/workspace/homework/103.txt', body);
+  }
+  else {console.error('request failed')}
+})
+
+request('http://visualizedata.github.io/datastructures/data/m04.html', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    fs.writeFileSync('/home/ubuntu/workspace/homework/104.txt', body);
+  }
+  else {console.error('request failed')}
+})
+
+request('http://visualizedata.github.io/datastructures/data/m05.html', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    fs.writeFileSync('/home/ubuntu/workspace/homework/105.txt', body);
+  }
+  else {console.error('request failed')}
+})
+
+request('http://visualizedata.github.io/datastructures/data/m06.html', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    fs.writeFileSync('/home/ubuntu/workspace/homework/106.txt', body);
+  }
+  else {console.error('request failed')}
+})
+
+request('http://visualizedata.github.io/datastructures/data/m07.html', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    fs.writeFileSync('/home/ubuntu/workspace/homework/107.txt', body);
+  }
+  else {console.error('request failed')}
+})
+
+request('http://visualizedata.github.io/datastructures/data/m08.html', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    fs.writeFileSync('/home/ubuntu/workspace/homework/108.txt', body);
+  }
+  else {console.error('request failed')}
+})
+
+request('http://visualizedata.github.io/datastructures/data/m09.html', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    fs.writeFileSync('/home/ubuntu/workspace/homework/109.txt', body);
+  }
+  else {console.error('request failed')}
+})
+
+request('http://visualizedata.github.io/datastructures/data/m10.html', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    fs.writeFileSync('/home/ubuntu/workspace/homework/110.txt', body);
+  }
+  else {console.error('request failed')}
+})
+
