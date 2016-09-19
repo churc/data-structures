72ab75e0115e1bb8b349ef373f365d2f82eb83cc
churc <ccinflux@hotmail.com>
2016-09-18 21:26:35 -0400

    Add files via upload

---
+++ homework3_churc/googleResults.js
@@ -0,0 +1,74 @@
+{
+   "results" : [
+      {
+				 "address_components" : [
+					{
+					   "long_name" : "63",
+					   "short_name" : "63",
+					   "types" : [ "street_number" ]
+					},
+					{
+					   "long_name" : "5th Avenue",
+					   "short_name" : "5th Ave",
+					   "types" : [ "route" ]
+					},
+					{
+					   "long_name" : "Lower Manhattan",
+					   "short_name" : "Lower Manhattan",
+					   "types" : [ "neighborhood", "political" ]
+					},
+					{
+					   "long_name" : "Manhattan",
+					   "short_name" : "Manhattan",
+					   "types" : [ "political", "sublocality", "sublocality_level_1" ]
+					},
+					{
+					   "long_name" : "New York",
+					   "short_name" : "New York",
+					   "types" : [ "locality", "political" ]
+					},
+					{
+					   "long_name" : "New York County",
+					   "short_name" : "New York County",
+					   "types" : [ "administrative_area_level_2", "political" ]
+					},
+					{
+					   "long_name" : "New York",
+					   "short_name" : "NY",
+					   "types" : [ "administrative_area_level_1", "political" ]
+					},
+					{
+					   "long_name" : "United States",
+					   "short_name" : "US",
+					   "types" : [ "country", "political" ]
+					},
+					{
+					   "long_name" : "10003",
+					   "short_name" : "10003",
+					   "types" : [ "postal_code" ]
+					}
+				 ],
+         "formatted_address" : "63 5th Ave, New York, NY 10003, USA",
+         "geometry" : {
+            "location" : {
+               "lat" : 40.7354188,
+               "lng" : -73.9936312
+            },
+            "location_type" : "ROOFTOP",
+            "viewport" : {
+               "northeast" : {
+                  "lat" : 40.7367677802915,
+                  "lng" : -73.99228221970849
+               },
+               "southwest" : {
+                  "lat" : 40.7340698197085,
+                  "lng" : -73.99498018029149
+               }
+            }
+         },
+         "place_id" : "ChIJBXO1P5hZwokRsVAg820Ubt8",
+         "types" : [ "street_address" ]
+      }
+   ],
+   "status" : "OK"
+}
