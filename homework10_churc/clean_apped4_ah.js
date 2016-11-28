d8ae1cf87d41d2edec3a0679cb500764c88b15f0
churc <ccinflux@hotmail.com>
2016-11-28 00:54:27 -0500

    Add files via upload

--- homework10_churc/clean_apped4_ah.js
+++ homework10_churc/clean_apped4_ah.js
@@ -1,10 +1,272 @@
 ///THIS WORKS - IT TAKES apped4_ah.js AND CLEANS IT UP 
-////THIS CAN BE SENT TO MONGODB
+////THIS CAN BE SENT TO MONGODB - see clean_apped6_ah.js
 
 var fs = require('fs');
 var async = require('async');
 
-var rawDataFile = fs.readFileSync('raw_groups1.txt');
+// var rawDataFile = fs.readFileSync('raw_groups1.txt');
+
+// var rawData = JSON.parse(rawDataFile);
+
+// var meetings = []; 
+
+// async.eachSeries(rawData, function(value, callback){
+//     var thisMeeting = new Object; 
+//     thisMeeting.building = value.building; 
+//     thisMeeting.name = value.name;
+//     thisMeeting.address = value.address;
+//     // thisMeeting.notes = value.notes;
+//     // thisMeeting.access = value.access; 
+//     for (var i=0; i < value.meetings.length; i++) {
+        
+//         var thisIt = value.meetings[i].trim();
+        
+//         if (thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') {
+//             thisMeeting.day = thisIt.substr(3, thisIt.indexOf(' ')-3);
+//         }
+        
+//         if (thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') { 
+//             thisMeeting.startTime = thisIt.split(' <b>')[0].substr(-8).trim(); 
+//         }
+        
+//           if (thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') {
+//             thisMeeting.endTime = thisIt.split(' <br>')[0].substr(-8).trim(); 
+//         }
+        
+//         else if (thisIt.substr(0, 10) == '<b>Meeting') {
+//             thisMeeting.meetingType = thisIt.split('</b>')[1].trim(); 
+//         }
+        
+//         else if (thisIt.substr(0, 10) == '<b>Special') {
+//             thisMeeting.specialInterest = thisIt.split('</b>')[1].trim(); 
+//         }
+        
+//         if (i > 0 & thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') {
+//             meetings.push(thisMeeting);
+//         }
+        
+//         if (i == value.meetings.length - 1) {
+//             meetings.push(thisMeeting);
+//         }
+//     }
+    
+//     thisMeeting.notes = value.notes;
+//     thisMeeting.access = value.access; 
+    
+//     callback();
+// });
+
+// function printIt () {
+//     for (var i=0; i < meetings.length; i++) {
+//         console.log(meetings[i]);
+//         console.log(meetings.length);
+//         fs.writeFileSync('groups1.txt', JSON.stringify(meetings));
+//     }
+// }
+
+// setTimeout(printIt, 1000); 
+
+
+
+////////////////
+
+// ///FOR GROUPS2 TXT FILE
+// var rawDataFile = fs.readFileSync('raw_groups2.txt');
+
+// var rawData = JSON.parse(rawDataFile);
+
+// var meetings = []; 
+
+// async.eachSeries(rawData, function(value, callback){
+//     var thisMeeting = new Object; 
+//     thisMeeting.building = value.building; 
+//     thisMeeting.name = value.name;
+//     thisMeeting.address = value.address;
+//     // thisMeeting.notes = value.notes;
+//     // thisMeeting.access = value.access; 
+//     for (var i=0; i < value.meetings.length; i++) {
+        
+//         var thisIt = value.meetings[i].trim();
+        
+//         if (thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') {
+//             thisMeeting.day = thisIt.substr(3, thisIt.indexOf(' ')-3);
+//         }
+        
+//         if (thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') { 
+//             thisMeeting.startTime = thisIt.split(' <b>')[0].substr(-8).trim(); 
+//         }
+        
+//           if (thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') {
+//             thisMeeting.endTime = thisIt.split(' <br>')[0].substr(-8).trim(); 
+//         }
+        
+//         else if (thisIt.substr(0, 10) == '<b>Meeting') {
+//             thisMeeting.meetingType = thisIt.split('</b>')[1].trim(); 
+//         }
+        
+//         else if (thisIt.substr(0, 10) == '<b>Special') {
+//             thisMeeting.specialInterest = thisIt.split('</b>')[1].trim(); 
+//         }
+        
+//         if (i > 0 & thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') {
+//             meetings.push(thisMeeting);
+//         }
+        
+//         if (i == value.meetings.length - 1) {
+//             meetings.push(thisMeeting);
+//         }
+//     }
+    
+//     thisMeeting.notes = value.notes;
+//     thisMeeting.access = value.access; 
+    
+//     callback();
+// });
+
+// function printIt () {
+//     for (var i=0; i < meetings.length; i++) {
+//         console.log(meetings[i]);
+//         console.log(meetings.length);
+//         fs.writeFileSync('groups2.txt', JSON.stringify(meetings));
+//     }
+// }
+
+// setTimeout(printIt, 1000); 
+
+
+
+// ///FOR GROUPS3 TXT FILE
+// var rawDataFile = fs.readFileSync('raw_groups3.txt');
+
+// var rawData = JSON.parse(rawDataFile);
+
+// var meetings = []; 
+
+// async.eachSeries(rawData, function(value, callback){
+//     var thisMeeting = new Object; 
+//     thisMeeting.building = value.building; 
+//     thisMeeting.name = value.name;
+//     thisMeeting.address = value.address;
+//     // thisMeeting.notes = value.notes;
+//     // thisMeeting.access = value.access; 
+//     for (var i=0; i < value.meetings.length; i++) {
+        
+//         var thisIt = value.meetings[i].trim();
+        
+//         if (thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') {
+//             thisMeeting.day = thisIt.substr(3, thisIt.indexOf(' ')-3);
+//         }
+        
+//         if (thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') { 
+//             thisMeeting.startTime = thisIt.split(' <b>')[0].substr(-8).trim(); 
+//         }
+        
+//           if (thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') {
+//             thisMeeting.endTime = thisIt.split(' <br>')[0].substr(-8).trim(); 
+//         }
+        
+//         else if (thisIt.substr(0, 10) == '<b>Meeting') {
+//             thisMeeting.meetingType = thisIt.split('</b>')[1].trim(); 
+//         }
+        
+//         else if (thisIt.substr(0, 10) == '<b>Special') {
+//             thisMeeting.specialInterest = thisIt.split('</b>')[1].trim(); 
+//         }
+        
+//         if (i > 0 & thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') {
+//             meetings.push(thisMeeting);
+//         }
+        
+//         if (i == value.meetings.length - 1) {
+//             meetings.push(thisMeeting);
+//         }
+//     }
+    
+//     thisMeeting.notes = value.notes;
+//     thisMeeting.access = value.access; 
+    
+//     callback();
+// });
+
+// function printIt () {
+//     for (var i=0; i < meetings.length; i++) {
+//         console.log(meetings[i]);
+//         console.log(meetings.length);
+//         fs.writeFileSync('groups3.txt', JSON.stringify(meetings));
+//     }
+// }
+
+// setTimeout(printIt, 1000); 
+
+
+
+// ///FOR GROUPS4 TXT FILE
+// var rawDataFile = fs.readFileSync('raw_groups4.txt');
+
+// var rawData = JSON.parse(rawDataFile);
+
+// var meetings = []; 
+
+// async.eachSeries(rawData, function(value, callback){
+//     var thisMeeting = new Object; 
+//     thisMeeting.building = value.building; 
+//     thisMeeting.name = value.name;
+//     thisMeeting.address = value.address;
+//     // thisMeeting.notes = value.notes;
+//     // thisMeeting.access = value.access; 
+//     for (var i=0; i < value.meetings.length; i++) {
+        
+//         var thisIt = value.meetings[i].trim();
+        
+//         if (thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') {
+//             thisMeeting.day = thisIt.substr(3, thisIt.indexOf(' ')-3);
+//         }
+        
+//         if (thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') { 
+//             thisMeeting.startTime = thisIt.split(' <b>')[0].substr(-8).trim(); 
+//         }
+        
+//           if (thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') {
+//             thisMeeting.endTime = thisIt.split(' <br>')[0].substr(-8).trim(); 
+//         }
+        
+//         else if (thisIt.substr(0, 10) == '<b>Meeting') {
+//             thisMeeting.meetingType = thisIt.split('</b>')[1].trim(); 
+//         }
+        
+//         else if (thisIt.substr(0, 10) == '<b>Special') {
+//             thisMeeting.specialInterest = thisIt.split('</b>')[1].trim(); 
+//         }
+        
+//         if (i > 0 & thisIt.substr(thisIt.indexOf(' ')-4, 4) == 'days') {
+//             meetings.push(thisMeeting);
+//         }
+        
+//         if (i == value.meetings.length - 1) {
+//             meetings.push(thisMeeting);
+//         }
+//     }
+    
+//     thisMeeting.notes = value.notes;
+//     thisMeeting.access = value.access; 
+    
+//     callback();
+// });
+
+// function printIt () {
+//     for (var i=0; i < meetings.length; i++) {
+//         console.log(meetings[i]);
+//         console.log(meetings.length);
+//         fs.writeFileSync('groups4.txt', JSON.stringify(meetings));
+//     }
+// }
+
+// setTimeout(printIt, 1000); 
+
+
+
+///FOR GROUPS5 TXT FILE
+var rawDataFile = fs.readFileSync('raw_groups5.txt');
 
 var rawData = JSON.parse(rawDataFile);
 
@@ -60,7 +322,7 @@ function printIt () {
     for (var i=0; i < meetings.length; i++) {
         console.log(meetings[i]);
         console.log(meetings.length);
-        fs.writeFileSync('groups1.txt', JSON.stringify(meetings));
+        fs.writeFileSync('groups5.txt', JSON.stringify(meetings));
     }
 }
 
