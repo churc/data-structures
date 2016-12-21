5d7df9f5125dbf7d3b086db7ead8a460b728910d
churc <ccinflux@hotmail.com>
2016-12-21 10:04:40 -0500

    Update apped1.js
    Scrapes Manhattan zones

--- Final Assignment 1/apped1.js
+++ Final Assignment 1/apped1.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 
 request('http://visualizedata.github.io/datastructures/data/m01.html', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    //fs.writeFileSync('/home/ubuntu/workspace/zipData/01.txt', body);
+    fs.writeFileSync('/home/ubuntu/workspace/zipData/01.txt', body);
   }
   else {console.error('request failed')}
 });
